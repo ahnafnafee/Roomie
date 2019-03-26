1e79084a2655b24752012d0798c926987cb88649
Ahnaf An Nafee <ahnafnafee@gmail.com>
2019-03-26 14:48:51 -0400

    Updated activities

---
+++ app/src/main/java/com/example/roomieprototype/CardStack.java
@@ -0,0 +1,17 @@
+package com.example.roomieprototype;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CardStack extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cardstack_expt);
+
+
+    }
+}
