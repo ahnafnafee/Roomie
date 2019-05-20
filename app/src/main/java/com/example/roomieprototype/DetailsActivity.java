3121d616cb698e083a9e4628261b3b704c57b8e9
Ahnaf An Nafee <ahnafnafee@gmail.com>
2019-05-20 06:51:33 -0400

    Added Image Slider in user_account

---
+++ app/src/main/java/com/example/roomieprototype/DetailsActivity.java
@@ -0,0 +1,137 @@
+package com.example.roomieprototype;
+
+import android.animation.ObjectAnimator;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.os.Bundle;
+import android.transition.Transition;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.ImageView;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import com.example.roomieprototype.imgutils.DecodeBitmapTask;
+
+public class DetailsActivity extends AppCompatActivity implements DecodeBitmapTask.Listener {
+
+    static final String BUNDLE_IMAGE_ID = "BUNDLE_IMAGE_ID";
+
+    private ImageView imageView;
+    private DecodeBitmapTask decodeBitmapTask;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        final int smallResId = getIntent().getIntExtra(BUNDLE_IMAGE_ID, -1);
+        if (smallResId == -1) {
+            finish();
+            return;
+        }
+
+        imageView = findViewById(R.id.image);
+        imageView.setImageResource(smallResId);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DetailsActivity.super.onBackPressed();
+            }
+        });
+
+        getWindow().getSharedElementEnterTransition().addListener(new Transition.TransitionListener() {
+
+            private boolean isClosing = false;
+
+            @Override
+            public void onTransitionPause(Transition transition) {
+            }
+
+            @Override
+            public void onTransitionResume(Transition transition) {
+            }
+
+            @Override
+            public void onTransitionCancel(Transition transition) {
+            }
+
+            @Override
+            public void onTransitionStart(Transition transition) {
+                if (isClosing) {
+                    addCardCorners();
+                }
+            }
+
+            @Override
+            public void onTransitionEnd(Transition transition) {
+                if (!isClosing) {
+                    isClosing = true;
+
+                    removeCardCorners();
+                    loadFullSizeBitmap(smallResId);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (isFinishing() && decodeBitmapTask != null) {
+            decodeBitmapTask.cancel(true);
+        }
+    }
+
+    private void addCardCorners() {
+        final CardView cardView = findViewById(R.id.card);
+        cardView.setRadius(25f);
+    }
+
+    private void removeCardCorners() {
+        final CardView cardView = findViewById(R.id.card);
+        ObjectAnimator.ofFloat(cardView, "radius", 0f).setDuration(50).start();
+    }
+
+    private void loadFullSizeBitmap(int smallResId) {
+        int bigResId;
+        switch (smallResId) {
+            case R.drawable.p1:
+                bigResId = R.drawable.p1_big;
+                break;
+            case R.drawable.p2:
+                bigResId = R.drawable.p2_big;
+                break;
+            case R.drawable.p3:
+                bigResId = R.drawable.p3_big;
+                break;
+            case R.drawable.p4:
+                bigResId = R.drawable.p4_big;
+                break;
+            case R.drawable.p5:
+                bigResId = R.drawable.p5_big;
+                break;
+            default:
+                bigResId = R.drawable.p1_big;
+        }
+
+        final DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getRealMetrics(metrics);
+
+        final int w = metrics.widthPixels;
+        final int h = metrics.heightPixels;
+
+        decodeBitmapTask = new DecodeBitmapTask(getResources(), bigResId, w, h, this);
+        decodeBitmapTask.execute();
+    }
+
+    @Override
+    public void onPostExecuted(Bitmap bitmap) {
+        imageView.setImageBitmap(bitmap);
+    }
+
+}
