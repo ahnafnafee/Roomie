fa66251f2b62896c0614fe6b34402b3ee6d57d77
Saharshv <thesaharsh@gmail.com>
2019-06-01 21:27:18 -0400

    Added chips validation in SignUp3

--- app/src/main/java/com/example/roomieprototype/signUp/SignUpActivity3.java
+++ app/src/main/java/com/example/roomieprototype/signUp/SignUpActivity3.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -34,6 +35,8 @@ public class SignUpActivity3 extends AppCompatActivity {
     private String temperatureSchedule;
     private String apartmentSchedule;
     private String dormSchedule;
+    private ChipGroup sleepChips,eatChips,cleanChips,studyChips,dormChips,apartmentChips,socialChips,temperatureChips;
+    private TextView chipError;
 
 
     @Override
@@ -50,43 +53,90 @@ public class SignUpActivity3 extends AppCompatActivity {
         // firebase user initiated
         user = mAuth.getCurrentUser();
 
+        // initializing error textview
+        chipError = findViewById(R.id.chips_error);
+
+        // initializing ChipGroups
+        sleepChips = findViewById(R.id.sleep_chips);
+        eatChips = findViewById(R.id.eat_chips);
+        cleanChips = findViewById(R.id.clean_chips);
+        studyChips = findViewById(R.id.study_chips);
+        dormChips = findViewById(R.id.dorm_chips);
+        apartmentChips = findViewById(R.id.apartment_chips);
+        socialChips = findViewById(R.id.social_chips);
+        temperatureChips = findViewById(R.id.temperature_chips);
+
         //Button Action
         Button btnSignUp = findViewById(R.id.signup_reg_button);
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                boolean cancel = false;
 
-                //Switch to MainActivity
-                Intent myIntent = new Intent(getBaseContext(), CardStack.class);
-                startActivity(myIntent);
+                // checking if all the chipgroups have a selected chip
+                if (sleepChips.getCheckedChipId() == View.NO_ID) {
+                    cancel = true;
+                    chipError.setText("Please select a sleeping Trait.");
+                } else if (eatChips.getCheckedChipId() == View.NO_ID) {
+                    cancel = true;
+                    chipError.setText("Please select an eating trait.");
+                } else if (cleanChips.getCheckedChipId() == View.NO_ID) {
+                    cancel = true;
+                    chipError.setText("Please select a cleaning trait.");
+                } else if (studyChips.getCheckedChipId() == View.NO_ID){
+                    cancel = true;
+                    chipError.setText("Please select a studying trait.");
+                }
+                else if (dormChips.getCheckedChipId()==View.NO_ID) {
+                    cancel = true;
+                    chipError.setText("Please select if you want a dorm or a suite.");
+                }
+                else if (apartmentChips.getCheckedChipId()==View.NO_ID) {
+                    cancel = true;
+                    chipError.setText("Please select if you have an apartment or not.");
+                }
+                else if (socialChips.getCheckedChipId()==View.NO_ID) {
+                    cancel = true;
+                    chipError.setText("Please select a social trait");
+                }
+                else if (temperatureChips.getCheckedChipId()==View.NO_ID) {
+                    cancel = true;
+                    chipError.setText("Please select a temperature trait.");
+                }
 
-                //Add Sleep Schedule to Firebase
-                sleepFireBase();
+                if(!cancel) {
+                    //Switch to MainActivity
+                    Intent myIntent = new Intent(getBaseContext(), CardStack.class);
+                    startActivity(myIntent);
 
-                //Add Clean Schedule to Firebase
-                cleanFireBase();
+                    //Add Sleep Schedule to Firebase
+                    sleepFireBase();
 
-                //Add Eat Schedule to Firebase
-                eatFireBase();
+                    //Add Clean Schedule to Firebase
+                    cleanFireBase();
 
-                //Add Study Schedule to Firebase
-                studyFireBase();
+                    //Add Eat Schedule to Firebase
+                    eatFireBase();
 
-                //Add Social to Firebase
-                socialFireBase();
+                    //Add Study Schedule to Firebase
+                    studyFireBase();
 
-                //Add Temperature to Firebase
-                temperaureFireBase();
+                    //Add Social to Firebase
+                    socialFireBase();
 
-                //Add apartment to firebase
-                apartmentFireBase();
+                    //Add Temperature to Firebase
+                    temperaureFireBase();
 
-                //Add dorm to firebase
-                dormFireBase();
+                    //Add apartment to firebase
+                    apartmentFireBase();
 
-                //Switch to MainActivity
-                Intent myIntent2 = new Intent(getBaseContext(), MatchingScreen.class);
-                startActivity(myIntent2);
+                    //Add dorm to firebase
+                    dormFireBase();
+
+                    //Switch to MainActivity
+                    Intent myIntent2 = new Intent(getBaseContext(), MatchingScreen.class);
+                    startActivity(myIntent2);
+                }
 
 
             }
